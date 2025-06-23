1b02f22cf286187310e403423988fd490e5e4953
jdavidIP <ibapol2003@gmail.com>
2025-06-23 15:12:02 -0400

    Add NavBar component and integrate it into the App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,19 @@ import MovieCard from "./components/MovieCard";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
 import Favourites from "./pages/Favourites";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favourites" element={<Favourites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
