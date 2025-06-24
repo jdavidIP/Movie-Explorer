b2e0ed900b71a2454401937896387b9f24c4b24c
jdavidIP <ibapol2003@gmail.com>
2025-06-23 22:49:26 -0400

    Change styling on favourites grid

--- frontend/src/pages/Favourites.jsx
+++ frontend/src/pages/Favourites.jsx
@@ -9,14 +9,16 @@ function Favourites() {
   return (
     <>
       {favourites.length > 0 ? (
-        <div className="favourites">
+        <div className="favorites">
           <h2>Your Favourites</h2>
-          {favourites.map((movie) => (
-            <MovieCard movie={movie} key={movie.id} />
-          ))}
+          <div className="movies-grid">
+            {favourites.map((movie) => (
+              <MovieCard movie={movie} key={movie.id} />
+            ))}
+          </div>
         </div>
       ) : (
-        <div className="favourites-empty">
+        <div className="favorites-empty">
           <h2>No favourites movies yet</h2>
           <p>
             Start adding movies to your favourites and they will appear here
